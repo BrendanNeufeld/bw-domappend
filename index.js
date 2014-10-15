166789cbdfaf967b2613f72ab32616c210647aff
Mikko Haapoja <me@mikkoh.com>
2014-10-15 12:05:31 -0400

    Made it work with string and html elements

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function( domSelector ) {
 
 	var parentEl = select( domSelector || 'body' );
 
-	return function( element ) {
+	return function( data ) {
 
 		return new promise( function( onOk, onErr ) {
 
@@ -14,9 +14,16 @@ module.exports = function( domSelector ) {
 				onErr( new Error( 'No html element to write into' ) );	
 			} else {
 
-				parentEl.appendChild( element );
+				if( typeof data == 'string' ) {
+
+					parentEl.innerHTML = parentEl.innerHTML + data;
+				} else {
+
+					parentEl.appendChild( data );	
+				}
 				
-				onOk( element );
+				
+				onOk( parentEl );
 			}
 		});
 	};
